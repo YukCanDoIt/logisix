2a7756bb31513eb0c30275f7245ae7def64de12a
Eunwoo Park <ewoo2821@gmail.com>
2024-12-10 18:53:22 +0900

    [Test] BaseEntity 상속한 p_user 생성 resolved: 13

--- common/src/main/java/com/sparta/common/entity/BaseEntity.java
+++ common/src/main/java/com/sparta/common/entity/BaseEntity.java
@@ -6,9 +6,7 @@ import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import java.sql.Timestamp;
 
 @EntityListeners(AuditingEntityListener.class)
