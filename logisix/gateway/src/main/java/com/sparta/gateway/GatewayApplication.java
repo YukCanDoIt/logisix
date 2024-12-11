6f4b7f1b93c248d23513ef970cdba66a98fe8c18
Eunwoo Park <ewoo2821@gmail.com>
2024-12-11 15:50:07 +0900

    [Feat] Config 정의

--- logisix/gateway/src/main/java/com/sparta/gateway/GatewayApplication.java
+++ logisix/gateway/src/main/java/com/sparta/gateway/GatewayApplication.java
@@ -2,8 +2,10 @@ package com.sparta.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.sparta.gateway", "com.sparta.common"})
 public class GatewayApplication {
 
 	public static void main(String[] args) {
