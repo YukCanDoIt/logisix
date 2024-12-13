3536a8b5f948f8176cb3130896534fb738a734b3
HanBeom98 <Carry9691@gmail.com>
2024-12-13 21:54:54 +0900

    help: 코드리뷰와 현 상황입니다

--- slack/src/main/java/com/sparta/slack/config/SlackConfig.java
+++ slack/src/main/java/com/sparta/slack/config/SlackConfig.java
@@ -1,11 +1,11 @@
-package com.sparta.order.config;
+package com.sparta.slack.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration
-public class RestTemplateConfig {
+public class SlackConfig {
 
   @Bean
   public RestTemplate restTemplate() {
