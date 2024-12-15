4a7ad83c04587b7306967a7b5d8a8f8666e61526
HanBeom98 <Carry9691@gmail.com>
2024-12-15 19:29:03 +0900

    refactor: BaseEntity 도입 및 글로벌 예외 처리 개선

---
+++ order/src/main/java/com/sparta/order/domain/BaseEntity.java
@@ -0,0 +1,39 @@
+package com.sparta.order.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@MappedSuperclass
+public abstract class BaseEntity {
+
+  @Column(name = "is_deleted", nullable = false)
+  private boolean isDeleted = false;
+
+  @Column(name = "is_delivery_started", nullable = false)
+  private boolean isDeliveryStarted = false;
+
+  @CreationTimestamp
+  @Column(name = "created_at", nullable = false, updatable = false)
+  private LocalDateTime createdAt;
+
+  @UpdateTimestamp
+  @Column(name = "updated_at")
+  private LocalDateTime updatedAt;
+
+  public void markAsDeleted() {
+    this.isDeleted = true;
+    this.updatedAt = LocalDateTime.now();
+  }
+
+  public void startDelivery() {
+    this.isDeliveryStarted = true;
+  }
+}
