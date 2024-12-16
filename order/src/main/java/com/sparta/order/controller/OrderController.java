4b0c4184c951b17b47a98d20c4ccd609a387d643
HanBeom98 <Carry9691@gmail.com>
2024-12-16 22:49:28 +0900

    refactor: OrderController 맵핑 수정

--- order/src/main/java/com/sparta/order/controller/OrderController.java
+++ order/src/main/java/com/sparta/order/controller/OrderController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 import java.util.UUID;
 
 @RestController
-@RequestMapping("/api/orders")
+@RequestMapping("/api/v1/orders")
 public class OrderController {
 
   private final OrderService orderService;
